67392202994841fc23bca11d115bf1b3359e25db
RiSer <sergiy19981@gmail.com>
2020-04-08 18:12:22 +0200

    update project

--- jarman/project.clj
+++ jarman/project.clj
@@ -1,9 +1,21 @@
 (defproject jarman "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
+  :description ""
+  :url ""
+  :license {:name "EPL-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.1"]]
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [seesaw "1.5.0"]
+                 [org.clojars.mjdowney/excel-clj "1.2.0"]
+                 [org.clojure/java.jdbc "0.7.10"]
+                 [mysql/mysql-connector-java "5.1.6"]]
   :main ^:skip-aot jarman.core
+
+  :repl-options {:init-ns hrtime.core}
   :target-path "target/%s"
-  :profiles {:uberjar {:aot :all}})
+  :profiles {:uberjar {:aot :all}}
+
+  :jar-name "jarman.jar"
+  :uberjar-name "jarman-standalone.jar"
+  
+  :java-source-paths ["src/java-src"]
+  :javac-options     ["-Xlint:unchecked"])

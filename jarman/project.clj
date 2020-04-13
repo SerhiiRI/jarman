df8ce75f4c2d9a1b3ed5358dccbd460b290edb05
Serhii Riznychuk <sergiy19981@gmail.com>
2020-04-13 02:24:56 +0200

    Create CLI tool for DB managment

--- jarman/project.clj
+++ jarman/project.clj
@@ -4,6 +4,7 @@
   :license {:name "EPL-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.1"]
+                 [org.clojure/tools.cli "1.0.194"]
                  [seesaw "1.5.0"]
                  [org.clojars.mjdowney/excel-clj "1.2.0"]
                  [org.clojure/java.jdbc "0.7.10"]

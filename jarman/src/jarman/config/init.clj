dbb290a0e647b9bd4ca334d42bfd12bd44ef5d5f
Serhii Riznychuk <sergiy19981@gmail.com>
2021-02-05 04:13:09 +0100

    end configuration swapping and translate swapping + gen translation

--- jarman/src/jarman/config/init.clj
+++ jarman/src/jarman/config/init.clj
@@ -2,6 +2,7 @@
   (:gen-class)
   (:use clojure.reflect
         seesaw.core)
+  (:import (java.io IOException))
   (:require [clojure.string :as string]
             [jarman.tools.lang :refer :all]
             [clojure.java.io :as io]))
@@ -9,8 +10,14 @@
 
 (def ^:dynamic *config-root* "./config")
 (def ^:dynamic *config-files* [[:init.edn][:resource.edn][:database.edn][:themes :jarman_light.edn][:themes :theme_config.edn]])
+;; (def *supported-languages* [:pl :ua])
+
 (def *configuration* (atom nil))
 (set-validator! *configuration* #(or (nil? %) (map? %)))
+
+(def *language* (atom nil))
+(set-validator! *language* #(or (nil? %) (map? %)))
+
 
 (defn block [m]
   {:type :block
@@ -31,60 +38,6 @@
 (defmacro is-file? [m] `(= (:type ~m) :file)) 
 (defmacro is-directory? [m] `(= (:type ~m) :directory)) 
 (defmacro is-error? [m] `(= (:type ~m) :error))
-
-(defn key-strings-path
-  [m & {:keys [sequence?] :or {sequence? false}}]
-  (blet 
-    (get-key-paths-recur
-     :map-part m
-     :end-path-func in-deep-key-path-f
-     :path nil
-     :sequence? sequence?)
-    @in-deep-key-path
-    [in-deep-key-path (atom [])
-     in-deep-key-path-f (fn [path]
-                          (if (and (string? (get-in m path)) (not= :value (last path)))
-                            (swap! in-deep-key-path (fn [path-list] (conj path-list (conj path (get-in m path)))))))]))
-
-
-
-
-(defn get-strings-from-config [m]
- (reduce (fn [acc p](assoc-in acc (butlast p) (last p))){}(key-strings-path m)))
-(defn get-strings-from-config-file [file]
-  (if-let [m (clojure.edn/read-string (slurp file))]
-    (reduce (fn [acc p](assoc-in acc (butlast p) (last p))){}(key-strings-path m))))
-
-
-(defn build-configuration [config-directory]
-  (letfn [(to-file [path] (apply (partial clojure.java.io/file config-directory) (map name path)))
-          (D [d v] {:name d :display? :edit :type :directory :value v})
-          (F [f v] {:name f :display? :edit :type :file :value v})]
-   ([:init.edn] 
-    [:resource.edn]
-    [:database.edn]
-    [:themes :jarman_light.edn]
-    [:themes :theme_config.edn])))
-
-(defn create-translation [config-directory]
-  (letfn [(to-file [path] (apply (partial clojure.java.io/file config-directory) (map name path)))
-          (D [d v]{:name d :display? :edit :type :directory :value v})
-          (F [f v]{:name f :display? :edit :type :file :value v})]
-    (reduce (fn [acc config-paths]
-             (let [f (to-file config-paths)]
-               (if (.exists f) acc
-                   (let [path (map name config-paths) plen (count path)
-                         swapped-config (get-strings-from-config-file f)]
-                     (cond
-                       (= plen 1) (merge acc (F (first path) swapped-config))
-                       (> plen 1) (merge acc (reduce #(D %2 %1) (F (first path) swapped-config) (butlast path))))))))
-            {}
-            [:init.edn] 
-            [:resource.edn]
-            [:database.edn]
-            [:themes :jarman_light.edn]
-            [:themes :theme_config.edn])))
-
 (defmacro is-keyword-list [kv-list]
   `(every? keyword? ~kv-list))
 (defmacro is-file-exist [string-path]
@@ -107,20 +60,92 @@
            (catch Exception e (EM (format "Error parsing configuration file '%s'" (str path))))))))
 
 (do
- (defn swapp-configuration [keyword-config-file-paths]
+  (defn swapp-*configuration*
+    ([] (swapp-*configuration* *config-files*))
+    ([keyword-config-file-paths]
+     (where
+      ((PF-list (<path|file>-list *config-root* keyword-config-file-paths))
+       (DM (fn [d v] {d {:name (str d) :display? :edit :type :directory :value v}}))
+       (FM (fn [f v] {f v})))
+      (reset!
+       *configuration*
+       (apply merge (for [[P F :as PF] PF-list
+                          :let [L (count PF)]] 
+                      (cond (= L 1) (load-config-file F)
+                            (> L 1) (reduce #(DM %2 %1) (FM (last P) (load-config-file F)) (butlast P))))))
+      nil)))
+  (swapp-*configuration* *config-files*))
+
+
+
+
+(defn key-strings-path
+  [m & {:keys [sequence?] :or {sequence? false}}]
+  (blet 
+    (get-key-paths-recur
+     :map-part m
+     :end-path-func in-deep-key-path-f
+     :path nil
+     :sequence? sequence?)
+    @in-deep-key-path
+    [in-deep-key-path (atom [])
+     in-deep-key-path-f (fn [path]
+                          (if (and (string? (get-in m path)) (not= :value (last path)))
+                            (swap! in-deep-key-path (fn [path-list] (conj path-list (conj path (get-in m path)))))))]))
+
+
+
+
+(defn get-strings-from-config [m]
+  (let [_TMP01 (atom {})]
+    (doall
+     (for [p (key-strings-path m)]
+       (swap! _TMP01 #(assoc-in % (butlast p) (last p)))))
+    @_TMP01))
+
+
+
+
+(defn save-cfg-to-file-pp [f m-cfg]
+  {:pre [(map? m-cfg)]}
+  (try (spit (clojure.java.io/file f) (with-out-str (clojure.pprint/pprint m-cfg)))
+       (catch IOException e (println (format "I/O error. Maybe problem in file '%s'" (str f))))
+       (catch Exception e (println (format "Configuration serialization problem for config: " (str m-cfg))))))
+(defn save-cfg-to-file [f m-cfg]
+  {:pre [(map? m-cfg)]}
+  (try (spit (clojure.java.io/file f) (pr-str m-cfg))
+       (catch IOException e (println (format "I/O error. Maybe problem in file '%s'" (str f))))
+       (catch Exception e (println (format "Configuration serialization problem for config: " (str m-cfg))))))
+
+
+(defn swapp-*language*
+  ([] (swapp-*language* *supported-languages*))
+  ([languages]
    (where
-    ((PF-list (<path|file>-list *config-root* keyword-config-file-paths))
-     (DM (fn [d v] {d {:name (str d) :display? :edit :type :directory :value v}}))
-     (FM (fn [f v] {f v})))
-    (apply merge (for [[P F :as PF] PF-list
-                       :let [L (count PF)]] 
-                   (cond (= L 1) (load-config-file F)
-                         (> L 1) (reduce #(DM %2 %1) (FM (last P) (load-config-file F)) (butlast P)))))))
- ;; (swapp-configuration *config-files*)
- )
+    ((language (load-config-file (clojure.java.io/file *config-root* "language.edn"))))
+    (reset! *language* language)
+    nil)))
+
+(defn config-lang-merge [m path-list-value]
+  (where
+   ((_TMP01 (atom m)))
+   (doall (for [px-t path-list-value
+          :let [path (vec (butlast px-t))]]
+      (if (nil? (get-in m path nil))
+        (swap! _TMP01 #(assoc-in % path (last px-t))))))
+   @_TMP01))
 
 
-
+(defn refresh-translation []
+  (where
+   ((original (load-config-file (clojure.java.io/file *config-root* "language.edn")))
+    (en-key-path-to-text (key-strings-path @*configuration*))
+    (fill-text (fn [CFG] (config-lang-merge CFG en-key-path-to-text))))
+   (-> original
+       (update-in [:en] fill-text)
+       (update-in [:pl] fill-text)
+       ((partial save-cfg-to-file-pp (clojure.java.io/file *config-root* "language.edn"))))
+   (swapp-*language*)))
 
 
 

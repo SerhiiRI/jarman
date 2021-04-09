8a5c307b947d23a80b84aeaf3a1e5be31cba82e7
Serhii Riznychuk <sergiy19981@gmail.com>
2021-04-09 11:46:56 +0200

    Create playground.clj

---
+++ jarman/src/jarman/logic/playground.clj
@@ -0,0 +1,186 @@
+(ns jarman.logic.playground
+  (:refer-clojure :exclude [update])
+  (:require
+   [jarman.logic.sql-tool :as toolbox :include-macros true :refer :all]
+   [jarman.logic.metadata :as metadata]
+   [jarman.config.storage :as storage]
+   [jarman.config.environment :as env]
+   [jarman.tools.lang :refer :all]
+   [clojure.data :as data]
+   [clojure.string :as string]
+   [clojure.java.jdbc :as jdbc])
+  (:import (java.util Date)
+           (java.text SimpleDateFormat)))
+
+
+(def ^:dynamic sql-connection {:dbtype "mysql"
+                               :host "127.0.0.1"
+                               :port 3306
+                               :dbname "jarman"
+                               :user "root"
+                               :password "1234"})
+
+;; (def ^:dynamic sql-connection {:dbtype "mysql"
+;;                                :host "192.168.1.69"
+;;                                :port 3306
+;;                                :dbname "jarman"
+;;                                :user "jarman"
+;;                                :password "dupa"})
+
+(def available-scheme ["service_contract"
+                       "seal"
+                       "repair_contract"
+                       "point_of_sale_group_links"
+                       "point_of_sale_group"
+                       "cache_register"
+                       "point_of_sale"
+                       "enterpreneur"
+                       "user"
+                       "permission"
+                       "METADATA"])
+
+(def METADATA
+  (create-table :METADATA
+                :columns [{:table [:varchar-100 :default :null]}
+                          {:prop [:text :default :null]}]))
+
+(def permission
+  (create-table :permission
+                :columns [{:permission_name [:varchar-20 :default :null]}
+                          {:configuration [:tinytext :nnull :default "'{}'"]}]))
+
+(def user
+  (create-table :user
+                :columns [{:login [:varchar-100 :nnull]}
+                          {:password [:varchar-100 :nnull]}
+                          {:first_name [:varchar-100 :nnull]}
+                          {:last_name [:varchar-100 :nnull]}
+                          {:id_permission [:bigint-120-unsigned :nnull]}]
+                :foreign-keys [{:id_permission :permission} {:delete :cascade :update :cascade}]))
+
+(def enterpreneur
+  (create-table :enterpreneur
+                :columns [{:ssreou [:tinytext :nnull]}
+                          {:ownership_form [:varchar-100 :default :null]}
+                          {:vat_certificate [:tinytext :default :null]}
+                          {:individual_tax_number [:varchar-100 :default :null]}
+                          {:director [:varchar-100 :default :null]}
+                          {:accountant [:varchar-100 :default :null]}
+                          {:legal_address [:varchar-100 :default :null]}
+                          {:physical_address [:varchar-100 :default :null]}
+                          {:contacts_information [:mediumtext :default :null]}]))
+
+(def point_of_sale
+  (create-table :point_of_sale
+                :columns [{:id_enterpreneur [:bigint-20-unsigned :default :null]}
+                          {:name [:varchar-100 :default :null]}
+                          {:physical_address  [:varchar-100 :default :null]}
+                          {:telefons  [:varchar-100 :default :null]}]
+                :foreign-keys [{:id_enterpreneur :enterpreneur} {:update :cascade}]))
+
+(def cache_register
+  (create-table :cache_register
+                :columns [{:id_point_of_sale [:bigint-20 :unsigned :default :null]}
+                          {:name [:varchar-100 :default :null]}
+                          {:serial_number [:varchar-100 :default :null]}
+                          {:fiscal_number [:varchar-100 :default :null]}
+                          {:manufacture_date [:date :default :null]}
+                          {:first_registration_date [:date :default :null]}
+                          {:is_working [:tinyint-1 :default :null]}
+                          {:version [:varchar-100 :default :null]}
+                          {:id_dev [:varchar-100 :default :null]}
+                          {:producer [:varchar-100 :default :null]}
+                          {:modem [:varchar-100 :default :null]}
+                          {:modem_model [:varchar-100 :default :null]}
+                          {:modem_serial_number [:varchar-100 :default :null]}
+                          {:modem_phone_number [:varchar-100 :default :null]}]
+                :foreign-keys [{:id_point_of_sale :point_of_sale} {:delete :cascade :update :cascade}]))
+
+(def point_of_sale_group
+  (create-table :point_of_sale_group
+                :columns [{:group_name [:varchar-100 :default :null]}
+                          {:information [:mediumtext :default :null]}]))
+
+(def point_of_sale_group_links
+  (create-table :point_of_sale_group_links
+                :columns [{:id_point_of_sale_group [:bigint-20-unsigned :default :null]}
+                          {:id_point_of_sale [:bigint-20-unsigned :default :null]}]
+                :foreign-keys [[{:id_point_of_sale_group :point_of_sale_group} {:delete :cascade :update :cascade}]
+                               [{:id_point_of_sale :point_of_sale}]]))
+
+(def seal
+  (create-table :seal
+                :columns [{:seal_number [:varchar-100 :default :null]}
+                          {:to_date [:date :default :null]}]))
+
+(def service_contract
+  (create-table :service_contract
+                :columns [{:id_point_of_sale [:bigint-20 :unsigned :default :null]}
+                          {:register_contract_date [:date :default :null]}
+                          {:contract_term_date [:date :default :null]}
+                          {:money_per_month [:int-11 :default :null]}]
+                :foreign-keys [{:id_point_of_sale :point_of_sale} {:delete :cascade :update :cascade}]))
+
+(def repair_contract
+  (create-table :repair_contract
+                :columns [{:id_cache_register [:bigint-20 :unsigned :default :null]}
+                          {:id_point_of_sale [:bigint-20 :unsigned :default :null]}
+                          {:creation_contract_date [:date :default :null]}
+                          {:last_change_contract_date [:date :default :null]}
+                          {:contract_terms_date [:date :default :null]}
+                          {:cache_register_register_date [:date :default :null]}
+                          {:remove_security_seal_date [:datetime :default :null]}
+                          {:cause_of_removing_seal [:mediumtext :default :null]}
+                          {:technical_problem [:mediumtext :default :null]}
+                          {:active_seal [:mediumtext :default :null]}]
+                :foreign-keys [[{:id_cache_register :cache_register} {:delete :cascade :update :cascade}]
+                               [{:id_point_of_sale :point_of_sale} {:delete :cascade :update :cascade}]]))
+
+
+(defmacro create-tabels [& tables]
+  `(do ~@(for [t tables]
+           `(jdbc/execute! sql-connection ~t))))
+(defmacro delete-tabels [& tables]
+  `(do ~@(for [t tables]
+           `(jdbc/execute! sql-connection (drop-table (keyword '~t))))))
+
+(defn create-scheme-one [scheme]
+  (eval `(jdbc/execute! sql-connection ~(symbol (string/join "/" ["jarman.schema-builder" (symbol scheme)])))))
+(defn create-scheme []
+  (create-tabels METADATA
+                 permission
+                 user
+                 enterpreneur
+                 point_of_sale
+                 cache_register
+                 point_of_sale_group
+                 point_of_sale_group_links
+                 repair_contract
+                 seal
+                 service_contract))
+
+(defn delete-scheme-one [scheme]
+  (eval `(jdbc/execute! sql-connection (drop-table ~(keyword scheme)))))
+(defn delete-scheme []
+  (delete-tabels service_contract
+                 seal
+                 repair_contract
+                 point_of_sale_group_links
+                 point_of_sale_group
+                 cache_register
+                 point_of_sale
+                 enterpreneur
+                 user
+                 permission
+                 METADATA))
+
+
+(defn regenerate-scheme []
+  (delete-scheme)
+  (create-scheme)
+  (metadata/do-create-meta))
+
+(defn regenerate-metadata []
+  (do (metadata/do-clear-meta)
+      (metadata/do-create-meta)))
+

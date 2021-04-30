c108575872fcbf0998f709dcafa9d61091104044
Aleks <32564717+Morfeu5z@users.noreply.github.com>
2021-04-30 03:43:55 +0200

    calendar upgrade

--- jarman/src/jarman/gui/gui_calendar.clj
+++ jarman/src/jarman/gui/gui_calendar.clj
@@ -7,10 +7,33 @@
         seesaw.font)
   (:import (java.text SimpleDateFormat))
   (:require [clojure.string :as string]
-            [jarman.gui.gui-tools :refer :all]))
+            [jarman.gui.gui-tools :refer :all :as gtool]
+            [jarman.gui.gui-components :refer :all :as gcomp]))
 
 (import 'jarman.test.Chooser)
 
+
+(defn date
+  "Remember that simple (date) ruturn current
+  date and time.
+  Also if you 
+
+  Example:
+  (date 1900 11 29 1 2 3) => 1900-12-29 01:02:03
+  (date 1900 11 29 1 2)   => 1900-12-29 01:02:00
+  (date 1900 11 29 1)     => 1900-12-29 01:00:00
+  (date 1900 11 29)       => 1900-12-29 00:00:00
+  (date 1900 11)          => 1900-12-01 00:00:00
+  (date 1900)             => 1900-01-01 00:00:00
+  (date)                  => 2020-02-29 19:07:41"
+  ([] (.format (java.text.SimpleDateFormat. "YYYY-MM-dd") (java.util.Date.)))
+  ([YYYY] (date YYYY 0 1 0 0 0))
+  ([YYYY MM] (date YYYY MM 1 0 0 0))
+  ([YYYY MM dd] (date YYYY MM dd 0 0 0))
+  ([YYYY MM dd hh] (date YYYY MM dd hh 0 0))
+  ([YYYY MM dd hh mm] (date YYYY MM dd hh mm 0))
+  ([YYYY MM dd hh mm ss] (.format (java.text.SimpleDateFormat. "YYYY-MM-dd")
+                                  (java.util.Date. (- YYYY 1900) MM dd hh mm ss))))
 
 (defn date-to-obj
   "Make shure, that your date in *data-format*
@@ -21,12 +44,12 @@
   (.parse (SimpleDateFormat. "yyyy-MM-dd") data-string))
 
 
-(def get-text-field 
-  (fn [data] (text :text data
-                   :background "#fff"
-                   :columns 20
-                   :border (compound-border (empty-border :left 10 :right 10 :top 5 :bottom 5)
-                                            (line-border :bottom 2 :color "#444444")))))
+;; (def get-text-field 
+;;   (fn [data] (text :text data
+;;                    :background "#fff"
+;;                    :columns 20
+;;                    :border (compound-border (empty-border :left 10 :right 10 :top 5 :bottom 5)
+;;                                             (line-border :bottom 2 :color "#444444")))))
 
 
 (defn get-calendar
@@ -38,18 +61,43 @@
   (Chooser/get_calendar textf (date-to-obj (text textf))))
 
 
-(def calendar-panel
-  (mig-panel
-   :constraints ["wrap 1" "100px[grow, center]100px" "30px[]30px"]
-   :items [[(get-calendar (get-text-field "2019-10-10"))]]))
+(defn calendar-panel
+  [& {:keys [set-date
+             editable?
+             enabled?
+             ]
+      :or {editable? false
+           enabled? true
+           set-date (date)}}]
+  (get-calendar (gcomp/input-text :v set-date :args [:enabled? enabled? :editable? editable?])))
 
+(defn calendar-with-atom
+  [& {:keys [changes
+             field
+             set-date
+             editable?
+             enabled?]
+      :or {changes (atom {})
+           field nil
+           set-date (date)
+           editable? false
+           enabled? true}}]
+  (get-calendar (gcomp/input-text
+                 :v set-date
+                 :args [:enabled? enabled?
+                        :editable? editable?
+                        :listen [:caret-update (fn [e]
+                                                 (if-not (nil? field) 
+                                                   (swap! changes (fn [storage] (assoc storage (keyword field) (value (to-widget e)))))))]])))
 
 (defn- frame-calendar []
   (frame :title "Jarman"
          :undecorated? false
          :resizable? false
          :minimum-size [1000 :by 760]
-         :content calendar-panel))
+         :content (mig-panel
+                   :constraints ["wrap 1" "100px[grow, center]100px" "30px[]30px"]
+                   :items [[(calendar-panel)]])))
 
  (-> (doto (frame-calendar) (.setLocationRelativeTo nil)) seesaw.core/pack! seesaw.core/show!)
 

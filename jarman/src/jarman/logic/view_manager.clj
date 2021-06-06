68d886e68fc5719f641494aba707614bd46fee21
SerhiiRi <sergiy19981@gmail.com>
2021-06-07 00:29:13 +0200

    fix datatoolkit and move to dedicated file whole pipeline

--- jarman/src/jarman/logic/view_manager.clj
+++ jarman/src/jarman/logic/view_manager.clj
@@ -2,21 +2,12 @@
   (:refer-clojure :exclude [update])
   (:require
    ;; Clojure toolkit 
-   ;; [clojure.data :as data]
    [clojure.string :as string]
    [clojure.java.io :as io]
    [seesaw.core :as c]
-   ;; [seesaw.util :as u]
    ;; ;; Jarman toolkit
-   [jarman.logic.document-manager :as doc]
    [jarman.logic.connection :as db]
    [jarman.tools.lang :include-macros true :refer :all]
-   ;; [jarman.gui.gui-tools :refer :all :as gtool]
-   ;; [jarman.resource-lib.icon-library :as ico]
-   ;; [jarman.tools.swing :as stool]
-   ;; [jarman.gui.gui-components :refer :all :as gcomp]
-   ;; [jarman.gui.gui-calendar :as calendar]
-   ;; [jarman.config.storage :as storage]
    [jarman.config.environment :as env]
    [jarman.plugin.jspl :refer :all :as jspl]
    [jarman.plugin.table :as plug]
@@ -25,110 +16,33 @@
   (:import (java.util Date)
            (java.text SimpleDateFormat)))
 
-(defn- tf-t-f [table-field]
-  (let [t-f (string/split (name table-field) #"\.")]
-    (mapv keyword t-f)))
 
-(defn- t-f-tf [table field]
-  (keyword (str (name table) "." (name (name field)))))
-
-;;; CONSTRUCTORS ;;;
-(defn metadata-toolkit-constructor [configuration toolkit-map]
-  (if-let [table-metadata (first (mt/getset! (:table-name configuration)))]
-    {:table-meta   ((comp :table :prop) table-metadata)
-     :columns-meta ((comp :columns :prop) table-metadata)}))
-
-(defn sql-crud-toolkit-constructor [configuration toolkit-map]
-  (let [rule-react-on (fn [k f] (fn [m] (if (get configuration k) (into m (f m)) m)))
-        m (first (mt/getset! (:table-name configuration)))
-        ;; relations (recur-find-path m)
-        override-fn! (fn [kdwd sql-fn]
-                       (let [override-action-expression (keyword (format "override-%s-expression" (name kdwd)))
-                             override-action (keyword (format "override-%s" (name kdwd)))]
-                        (fn [m] (if (and (some? (kdwd configuration)) (not= (kdwd configuration) :none))
-                                 {override-action-expression (fn [e] (sql-fn ((kdwd configuration) e)))
-                                  override-action (fn [e] (db/exec (sql-fn ((kdwd configuration) e))))}
-                                 {}))))
-        rule-insert! (rule-react-on :insert (override-fn! :insert insert!))
-        rule-update! (rule-react-on :update (override-fn! :update update!))
-        rule-delete! (rule-react-on :insert (override-fn! :insert delete!))
-        id_column (t-f-tf (:table-name configuration) :id)
-        table-name ((comp :field :table :prop) m)
-        columns (map :field ((comp :columns :prop) m))
-        update-expression (fn [entity] (if (id_column entity)  (update table-name :set entity :where (=-v id_column (id_column entity)))))
-        insert-expression (fn [entity] (if (nil? (id_column entity)) (insert table-name :set entity)))
-        delete-expression (fn [entity] (if (id_column entity) (delete table-name :where (=-v id_column (id_column entity)))))
-        select-expression (fn [& {:as args}]
-                            (apply (partial select-builder (:table-name configuration))
-                                   (mapcat vec (into (:query configuration) args))))]
-    (-> {:update-expression update-expression
-         :insert-expression insert-expression
-         :delete-expression delete-expression
-         :select-expression select-expression
-         :update (fn [e] (db/exec (update-expression e)))
-         :insert (fn [e] (db/exec (insert-expression e)))
-         :delete (fn [e] (db/exec (delete-expression e)))
-         :select (fn [ ] (db/query (select-expression)))
-         :model-id id_column}
-        rule-insert!
-        rule-update!
-        rule-delete!)))
-
-
-#_(let [configuration {:table-name :sealn
-                     :name "Pushing seals"
-                     :tables [:seal]
-                     :view [:seal.seal_number
-                            :seal.to_date]
-                     :override-model [{:start-value :text-input} {:end-value :text-input}]
-                     :insert (fn [m] {:table-name :seal
-                                     :column-list [:seal.seal_number :seal.to_date]
-                                     :values (mapv #(vector % (:to-date m)) (range (:start-value m) (+ (:end-value m) 1)))}) 
-                     :update (fn [m] {:table-name :seal
-                                     :set m}) ;;:none
-                     :delete :none
-                     :query {:column (as-is :seal.id :seal.seal_number :seal.to_date)}}]
-  (keys (sql-crud-toolkit-constructor configuration {})))
-
-(defn export-toolkit-constructor [configuration toolkit-map]
-  (if-let [select-expression (:select-expression toolkit-map)]
-    {:export-select-expression (fn [] (select-expression :column nil :inner-join nil :where nil))
-     :export-select (fn [] (db/query (select-expression :column nil :inner-join nil :where nil)))}))
-
-(defn document-toolkit-constructor [configuration toolkit-map]
-  (let [table-name (:table-name configuration)]
-    (doc/select-documents-by-table table-name)))
-
-(defn data-toolkit-pipeline [configuration]
-  (let [rule-react-on (fn [f & ks] (fn [m] (if (every? (fn [k] (some? (k configuration))) ks) (into m (f configuration m)) m)))
-        sql-crud-toolkit (rule-react-on sql-crud-toolkit-constructor :query)
-        metadata-toolkit (rule-react-on metadata-toolkit-constructor :table-name)
-        export-sql-toolkit (rule-react-on export-toolkit-constructor :query)
-        document-toolkit (rule-react-on document-toolkit-constructor :table-name)]
-    (-> {} sql-crud-toolkit metadata-toolkit
-        export-sql-toolkit document-toolkit)))
+(defn- toolkit-pipeline-reducer [configuration & toolkit-list]
+  (reduce (fn [acc-toolkit toolkit-pipeline]
+            (toolkit-pipeline configuration acc-toolkit))
+          {}
+          toolkit-list))
 
 (def ^:private views-configuration+toolkit (atom {}))
 (defn views-configuration+toolkit-get []
   @views-configuration+toolkit)
-(defn views-configuration+toolkit-set [keyword-table-name plugin-name plugin-configuration data-toolkit]
+(defn views-configuration+toolkit-set [keyword-table-name plugin-name plugin-configuration toolkit]
   {:pre [(keyword? keyword-table-name)]}
   (swap! views-configuration+toolkit
          (fn [m] (assoc-in m [keyword-table-name plugin-name]
                           {:configuration plugin-configuration
-                           :data-toolkit data-toolkit}))) nil)
+                           :toolkit toolkit}))) nil)
 
 (defn get-parameters [data]
-  (loop [prm {}
-         l data
-         plugins []]
+  (loop [prm {} l data plugins []]
     (if (not-empty l)
       (cond
         (keyword? (first l)) (recur (into prm {(first l) (second l)}) (drop 2 l) plugins)
         (list? (first l)) (recur prm (drop 1 l) (conj plugins (first l)))
         :else (recur prm (drop 1 l) plugins))
-      [(if-not (contains? prm :pkey) (assoc prm :pkey [:user]))
+      [(if-not (contains? prm :permision) (assoc prm :permission [:user]))
        plugins])))
+
 
 (defmacro defview-debug [table-model-name & body]
   (let [[conf-prms fbody] (get-parameters body)
@@ -158,7 +72,7 @@
                     ktable# ~(keyword table-model-name)
                     kplugin# ~(keyword f)
                     plugin-configuration# (get cfg# kplugin#)
-                    plugin-data-toolkit#  (data-toolkit-pipeline (get cfg# kplugin#))]
+                    plugin-data-toolkit#  (toolkit-pipeline-reducer (get cfg# kplugin#) [data-toolkit-pipeline])]
                 (views-configuration+toolkit-set ktable#
                                                  kplugin#
                                                  plugin-configuration#
@@ -266,7 +180,6 @@
       "Error with file"
       (binding [*ns* (find-ns 'jarman.logic.view-manager)] 
         (doall (map (fn [x] (eval x)) data))))))
-
 
 
 ;; (defview
---
+++ jarman/src/jarman/plugin/data_toolkit.clj
@@ -0,0 +1,223 @@
+(ns jarman.plugin.data-toolkit
+  (:refer-clojure :exclude [update])
+  (:require
+   ;; Clojure toolkit 
+   [clojure.string :as string]
+   [clojure.java.io :as io]
+   [seesaw.core :as c]
+   ;; Jarman toolkit
+   [jarman.logic.document-manager :as doc]
+   [jarman.logic.connection :as db]
+   [jarman.tools.lang :include-macros true :refer :all]
+   [jarman.config.environment :as env]
+   [jarman.logic.sql-tool :as toolbox :include-macros true :refer :all]
+   [jarman.logic.metadata :as mt])
+  (:import (java.util Date)
+           (java.text SimpleDateFormat)))
+
+;;;;;;;;;;;;;;;;;;;
+;;; SQL TOOLKIT ;;;
+;;;;;;;;;;;;;;;;;;;
+
+(defn- tf-t-f [table-field]
+  (let [t-f (string/split (name table-field) #"\.")]
+    (mapv keyword t-f)))
+
+(defn- t-f-tf [table field]
+  (keyword (str (name table) "." (name (name field)))))
+
+;;; Helper sql-functionality ;;; 
+(defn- sql-operation-suffix [keyword-rule-name suffix] {:pre [(keyword? keyword-rule-name) (keyword? suffix)]}
+  (string/ends-with? (name keyword-rule-name) (name suffix)))
+
+(defn- sql-operation-expression [keyword-rule-name] {:pre [(keyword? keyword-rule-name)]}
+  (keyword (format "%s-%s" (name keyword-rule-name) "expression")))
+
+(defn- sql-make-select-wrapper
+  "Description
+     Take keyword as idintificator and take lambda or keyword
+    for example {:table-name \"user\"} or #(:table-name (:t-name %))
+    and wrapp into `select!` function which generate SQL syntax
+     Function return two key first has name as `keyword-rule-name`
+    which do jdbc request, second has name with `-expression` on
+    end for debug aims and return only SQL syntax expression
+  
+  Example 
+    (sql-make-select-wrapper :some-test-select {:table-name \"user\"})
+    ;;=> {:some-test-select (fn [e]...) :some-test-select-expression (fn [e] ...) "
+  [keyword-rule-name query-settings]
+  {:pre [(keyword? keyword-rule-name)]}
+  (where
+   ((rule-expression  keyword-rule-name do sql-operation-expression)
+    (rule-name        keyword-rule-name)
+    (query-lambda-expression query-settings if2 map?
+                             (fn [e] (select! query-settings))
+                             (fn [e] (select! (query-settings e))))
+    (query-lambda (fn [e] (db/query (query-lambda-expression e)))))
+   {rule-expression query-lambda-expression
+    rule-name       query-lambda}))
+
+(defn- sql-make-wrapper-for
+  "Description
+     Take keyword as idintificator and take lambda for example
+    #(:table-name (:t-name %)) and wrapp into `sql-operation`
+    function which generate SQL syntax
+     Function return two key first has name as `keyword-rule-name`
+    which do jdbc request, second has name with `-expression` on
+    end for debug aims and return only SQL syntax expression
+  
+  Example 
+    (sql-make-wrapper-for insert! :user-insert {:table-name \"user\"})
+    ;;=> {:user-insert (fn [e]...) :user-insert-expression (fn [e] ...) "
+  [sql-operation keyword-rule-name query-lambda]
+  {:pre [(keyword? keyword-rule-name) (fn? query-lambda)]}
+  (where
+   ((rule-expression  keyword-rule-name do sql-operation-expression)
+    (rule-name        keyword-rule-name)
+    (query-lambda-expression (fn [e] (sql-operation (query-lambda e))))
+    (query-lambda            (fn [e] (db/exec (query-lambda-expression e)))))
+   {rule-expression query-lambda-expression
+    rule-name       query-lambda}))
+
+(def ^:private sql-make-insert-wrapper (partial sql-make-wrapper-for insert!))
+(def ^:private sql-make-delete-wrapper (partial sql-make-wrapper-for delete!))
+(def ^:private sql-make-update-wrapper (partial sql-make-wrapper-for update!))
+
+(defn- response-f-on-suffix
+  "Example
+  
+  (where ((configuration {:test-m-select {:table-name \"user\"}
+                        :test-select (fn [{login :login}] {:table-name \"user\" :where [:= login :login]})
+                        :test-insert (fn [e] {:table-name \"user\" :values e})
+                        :test-update (fn [{login :login password :password :as e}] {:table-name \"user\" :set e :where [:= :login login]})
+                        :test-delete (fn [{login :login}] {:table-name \"user\" :where [:= :login login]})
+                        :must-not-reaction-on-this-key nil})
+        (toolkit (reduce
+                  (fn [m-acc [k v]]
+                    (-> m-acc
+                        (supply-insert k v)
+                        (supply-update k v)
+                        (supply-delete k v)
+                        (supply-select k v))) {} configuration))
+        (test-user {:login \"admin\" :password \"12345\"}))
+       {:test-m-select ((:test-m-select-expression toolkit) nil)
+        :test-select ((:test-select-expression toolkit) test-user)
+        :test-insert ((:test-insert-expression toolkit) test-user)
+        :test-update ((:test-update-expression toolkit) test-user)
+        :test-delete ((:test-delete-expression toolkit) test-user)})
+   ;;=> 
+      {:test-m-select \"SELECT * FROM user\",
+       :test-select \"SELECT * FROM user WHERE \\\"admin\\\" = login\",
+       :test-insert \"INSERT `user` SET login=\\\"admin\\\", password=\\\"12345\\\"\",
+       :test-update \"DELETE user WHERE login = \\\"admin\\\"\",
+       :test-delete \"UPDATE `user` WHERE login = \\\"admin\\\"\"}"
+  [suffix sql-operation acc-m sql-query-key configuration]
+  (if (sql-operation-suffix sql-query-key suffix)
+    (into acc-m (sql-operation sql-query-key configuration)) acc-m))
+
+;;; This function list add some toolkit to the map, reacting on ending suffix pattern
+(def ^:private supply-select (partial response-f-on-suffix :select sql-make-select-wrapper))
+(def ^:private supply-insert (partial response-f-on-suffix :insert sql-make-insert-wrapper))
+(def ^:private supply-update (partial response-f-on-suffix :update sql-make-delete-wrapper))
+(def ^:private supply-delete (partial response-f-on-suffix :delete sql-make-update-wrapper))
+
+(defn- sql-crud-toolkit-constructor
+  "Description
+    Generate datatoolkit for sql expression
+  
+  Example
+    (sql-crud-toolkit-constructor
+       {:table-name :repair_contract 
+        :name \"repair_contract\"
+        :plug-place [:#tables-view-plugin]
+        :tables [:repair_contract]
+        :user-all-select {:table-name \"user\"}
+        :model [:repair_contract.id]
+        :query {}} {})
+    ;;=> 
+      {:model-id :repair_contract.id,
+       :insert (fn [..] ...),
+       :delete-expression (fn [..] ...),
+       :select-expression (fn [..] ...),
+       :update (fn [..] ...),
+       :delete (fn [..] ...),
+       :update-expression (fn [..] ...),
+       :insert-expression (fn [..] ...),
+       :user-all-select (fn [..] ...),
+       :user-all-select-expression (fn [..] ...),
+       :select (fn [..] ...)}"
+  [configuration toolkit-map]
+  (where ((table-metadata configuration do :table-name do mt/getset! do first)
+          (id_column (t-f-tf (:table-name configuration) :id))
+          (table-name ((comp :field :table :prop) table-metadata))
+          (columns ((comp :columns :prop) table-metadata) map :field) 
+          (update-expression (fn [entity] (if (id_column entity)  (update table-name :set entity :where (=-v id_column (id_column entity))))))
+          (insert-expression (fn [entity] (if (nil? (id_column entity)) (insert table-name :set entity))))
+          (delete-expression (fn [entity] (if (id_column entity) (delete table-name :where (=-v id_column (id_column entity))))))
+          (select-expression (fn [& {:as args}]
+                               (select!
+                                (merge
+                                 (if (:table-name (:query configuration))
+                                   (:query configuration)
+                                   (into (:query configuration)
+                                         {:table-name (:table-name configuration)}))
+                                 args)))))
+         (where
+          ((standart-crud-m
+            {:update-expression update-expression
+             :insert-expression insert-expression
+             :delete-expression delete-expression
+             :select-expression select-expression
+             :update (fn [e] (db/exec (update-expression e)))
+             :insert (fn [e] (db/exec (insert-expression e)))
+             :delete (fn [e] (db/exec (delete-expression e)))
+             :select (fn [ ] (db/query (select-expression)))
+             :model-id id_column}))
+          (reduce
+           (fn [m-acc [k v]]
+             (-> m-acc
+                 (supply-insert k v)
+                 (supply-update k v)
+                 (supply-delete k v)
+                 (supply-select k v)))
+           standart-crud-m
+           configuration))))
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;
+;;; METADATA TOOLKIT ;;;
+;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defn- metadata-toolkit-constructor [configuration toolkit-map]
+  (if-let [table-metadata (first (mt/getset! (:table-name configuration)))]
+    {:table-meta   ((comp :table :prop) table-metadata)
+     :columns-meta ((comp :columns :prop) table-metadata)}))
+
+;;;;;;;;;;;;
+;;; TODO ;;;
+;;;;;;;;;;;;
+
+(defn- export-toolkit-constructor [configuration toolkit-map]
+  (if-let [select-expression (:select-expression toolkit-map)]
+    {:export-select-expression (fn [] (select-expression :column nil :inner-join nil :where nil))
+     :export-select (fn [] (db/query (select-expression :column nil :inner-join nil :where nil)))}))
+
+(defn- document-toolkit-constructor [configuration toolkit-map]
+  (let [table-name (:table-name configuration)]
+    (doc/select-documents-by-table table-name)))
+
+;;;;;;;;;;;;;;;;
+;;; PIPELINE ;;;
+;;;;;;;;;;;;;;;;
+
+(defn data-toolkit-pipeline [configuration other-toolkit-map]
+  (let [rule-react-on (fn [f & ks] (fn [m] (if (every? (fn [k] (some? (k configuration))) ks) (into m (f configuration m)) m)))
+        sql-crud-toolkit (rule-react-on sql-crud-toolkit-constructor :query)
+        metadata-toolkit (rule-react-on metadata-toolkit-constructor :table-name)
+        export-sql-toolkit (rule-react-on export-toolkit-constructor :query)
+        document-toolkit (rule-react-on document-toolkit-constructor :table-name)]
+    (-> other-toolkit-map
+        sql-crud-toolkit
+        metadata-toolkit
+        export-sql-toolkit
+        document-toolkit)))

5a0b80ed974cf52e101d20ef43e32937d43da335
Serhii Riznychuk <sergiy19981@gmail.com>
2021-06-22 06:44:22 +0200

    redefine global plugin spec

--- jarman/src/jarman/plugin/spec.clj
+++ jarman/src/jarman/plugin/spec.clj
@@ -4,31 +4,18 @@
    [jarman.config.environment :as env]
    [jarman.tools.lang :refer :all :as lang]))
 
-(s/def :common-plugin/vector vector?) 
-(s/def :common-plugin/keyword keyword?)
-(s/def :common-plugin/string string?)
+(s/def :jarman.plugin.spec/keyword-list (s/and sequential? #(every? keyword? %)))
+(s/def :jarman.plugin.spec/permission :jarman.plugin.spec/keyword-list)
+(s/def :jarman.plugin.spec/plug-place :jarman.plugin.spec/keyword-list)
+(s/def :jarman.plugin.spec/name       string?)
 
-;; spec for permission
-(s/def :global-plugin/permission (s/and :common-plugin/vector (s/coll-of :common-plugin/keyword)))
-;;(s/valid? :global-plugin/permission [:user :admin]) ;;=> true
+;;; DEPRECATED --  @julia must look
+;; (defn test-keys-jtable [conf spec-map]
+;;   (s/def :global-plugin/block-table
+;;     (eval spec-map))
+;;   (let [resault (s/valid? :global-plugin/block-table conf)]
+;;     (if resault
+;;       resault
+;;       (do (s/explain :global-plugin/block-table conf)
+;;           resault))))
 
-(s/def :global-plugin/name :common-plugin/string)
-(s/def :global-plugin/plug-place :common-plugin/vector)
-
-(s/def :global-plugin/block-table
-  (s/keys :req-un [:global-plugin/permission
-                   :global-plugin/name
-                   :global-plugin/plug-place]
-          ;; :opt-un [:]
-          ))
-
-(defn test-keys-jtable [conf spec-map]
-  (s/def :global-plugin/block-table
-    (eval spec-map))
-  (let [resault (s/valid? :global-plugin/block-table conf)]
-    (if resault
-      resault
-      (do (s/explain :global-plugin/block-table conf)
-          resault))))
-
-;; TODO: Add spec for model, etc

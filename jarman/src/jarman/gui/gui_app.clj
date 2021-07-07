911d0282df0638138010f32dafbe4be3f0ef130a
Aleks <32564717+Morfeu5z@users.noreply.github.com>
2021-07-07 12:49:35 +0200

    Moved periods to plugin

--- jarman/src/jarman/gui/gui_app.clj
+++ jarman/src/jarman/gui/gui_app.clj
@@ -48,7 +48,7 @@
      Complete component
    "
   (fn [] (if (= false (:valid? (cm/get-in-segment []))) 
-           (do (println "[ Warning ] Can not loading configs: " (:output (cm/get-in-segment [])))
+           (do ;; (println "[ Warning ] Can not loading configs: " (:output (cm/get-in-segment [])))
                [(gcomp/button-expand "Settings Error" []
                                :ico (stool/image-scale icon/alert-64-png 25)
                                :ico-hover (stool/image-scale icon/alert-64-png 25)
@@ -174,7 +174,7 @@
                        :id :app-functional-space
                        :background (new Color 0 0 0 0)
                        :items (gtool/join-mig-items array))]
-      (println tabs)
+      ;; (println tabs)
       (state/set-state :jarman-views-service (vs/new-views-service tabs-space views-space))
       (mig-panel
        :id :operation-space
@@ -184,113 +184,6 @@
        ;; :border (line-border :left 1 :color "#999")
        :items [[(gcomp/min-scrollbox tabs-space :vscroll :never)]
                [views-space]]))))
-
-;; ((state/state :jarman-views-service) :reload :view-id (keyword "DB Visualiser"))
-;; ((state/state :jarman-views-service) :get-all-view) 
-(defn create-period--period-form
-  []
-  (gcomp/vmig
-   :vrules "[fill][100, shrink 0, fill][grow, fill]"
-   :items [[(gcomp/header-basic "Okresy")]
-           [(gcomp/min-scrollbox
-             (mig-panel :constraints ["wrap 4" "10px[fill][fill]50px[fill][fill]10px" "10px[fill]10px"]
-                        :items [[(c/label :text "Organization:")]
-                                [(c/label :text "Frank & Franky Co." :border (b/line-border :bottom 1 :color "#494949"))]
-                                [(c/label :text "Time:")]
-                                [(c/label :text "12/03/2021 - 11/03/2022"  :border (b/line-border :bottom 1 :color "#494949"))]
-                                [(c/label :text "Customer:")]
-                                [(c/label :text "Franklyn Badabumc" :border (b/line-border :bottom 1 :color "#494949"))]
-                                [(c/label :text "Full amount:")]
-                                [(c/label :text "7000,-" :border (b/line-border :bottom 1 :color "#494949"))]
-                                [(c/label :text "Service:")]
-                                [(c/label :text "Mr. Jarman" :border (b/line-border :bottom 1 :color "#494949"))]])
-             :vscroll :never)]
-           [(gcomp/vmig
-             :vrules "[fill]0px[grow, fill]"
-             :items [[(gcomp/menu-bar
-                       {:justify-end true
-                        :id :menu-for-period-table
-                        :buttons [[(gtool/get-lang-btns :save) icon/agree1-blue-64-png "" (fn [e])]
-                                  [(gtool/get-lang-btns :export) icon/excel-64-png "" (fn [e])]]})]
-                     [(c/scrollable (seesaw.swingx/table-x :model [:columns ["Servise month" "Amount" "Payment status"] :rows [["03/2021" "2500,-" "FV: 042/03/2021"]
-                                                                                                                             ["04/2021" "2000,-" "FV: 042/04/2021"]
-                                                                                                                             ["05/2021" "2500,-" "Expected payment"]]]))]])]]))
-
-(defn get-period-list
-  [company-id]
-  (cond
-    (= company-id 1) {}))
-
-(defn create-period--period-list
-  [list-space view-space return-fn company-id]
-  (let [;; period-list (get-period-list company-id)
-        auto-menu-hide false
-        return (gcomp/button-slim (str "<< " (gtool/get-lang-btns :back))
-                                  :underline-size 1
-                                  :onClick (fn [e] (c/invoke-later (do
-                                                                   (c/config! list-space :items (gtool/join-mig-items (return-fn list-space view-space return-fn)))
-                                                                   (gtool/switch-focus)))))
-        periods (gcomp/expand-form-panel
-                 list-space
-                 [return
-                  (gcomp/scrollbox
-                   (gcomp/vmig
-                    :items (gtool/join-mig-items
-                            (gcomp/vmig
-                             :items (gtool/join-mig-items
-                                     (gcomp/button-slim "01/01/2021 - 31/12/2021"
-                                                        :onClick (fn [e]
-                                                                   (c/config! view-space :items (gtool/join-mig-items (create-period--period-form)))
-                                                                   (if auto-menu-hide ((:hide-show (c/config (c/select list-space [:#expand-panel]) :user-data))))))
-                                     (gcomp/button-slim "01/01/2021 - 31/12/2021" :onClick (fn [e]))
-                                     (gcomp/button-slim "01/01/2021 - 31/12/2021" :onClick (fn [e]))
-                                     (gcomp/button-slim "01/01/2021 - 31/12/2021" :onClick (fn [e]))
-                                     (gcomp/button-slim "01/01/2021 - 31/12/2021" :onClick (fn [e])))))))]
-                 :max-w 180
-                 :args [:id :expand-panel :background "#fff"])]
-    (gtool/set-focus return)
-    return
-    periods))
-
-
-(defn get-company-list
-  [] [{:name "Trashpanda-Team" :id 1} {:name "Frank & Franky" :id 3}
-      {:name "Trashpanda-Team" :id 1} {:name "Trashpandowe Zakłady Wyrobów Kodowych Team" :id 3}
-      {:name "Trashpanda-Team" :id 1} {:name "Frank & Franky" :id 3}])
-
-(defn create-period--period-companys-list
-  [list-space view-space return-fn] ;; [{:name "Frank & Franky" :id 3}]
-  (gtool/rm-focus)
-  (let
-   [model (get-company-list)
-    buttons (map (fn [enterpreneur]
-                   (let [name (:name enterpreneur)
-                         btn (gcomp/button-slim name
-                                                :onClick (fn [e] (c/invoke-later (do
-                                                                                 (c/config! list-space :items (gtool/join-mig-items (create-period--period-list list-space view-space return-fn (get enterpreneur :id))))
-                                                                                 (gtool/switch-focus))))
-                                                :args [:tip name])]
-                     (gtool/set-focus-if-nil btn)
-                     btn))
-                 model)]
-    (gcomp/expand-form-panel
-     list-space
-     (gcomp/vmig :items (gtool/join-mig-items buttons))
-     :max-w 180
-     :args [:background "#fff"])))
-
-
-(defn create-period-view
-  []
-  (let [list-space (gcomp/vmig :args [:border (b/line-border :left 1 :right 1 :color "#eee")])
-        view-space (gcomp/vmig)
-        list-space (c/config! list-space :items (gtool/join-mig-items (create-period--period-companys-list list-space view-space create-period--period-companys-list)))]
-    (gcomp/hmig
-     :hrules "[shrink 0, fill]0px[grow, fill]"
-     :items [[list-space]
-             [view-space]]
-     :args [:background "#fff"])))
-
 
 
 (def jarmanapp
@@ -319,7 +212,7 @@
                                                         ])]
                                        [(gcomp/button-expand "Tables" [] :id :tables-view-plugin :expand :yes)]
                                       
-                                       [(gcomp/button-expand "Okresy" [(gcomp/button-expand-child "Okresy" :onClick (fn [e] ((state/state :jarman-views-service) :set-view :view-id "okresy" :title "Okresy" :component-fn create-period-view)))])]
+                                       ;; [(gcomp/button-expand "Okresy" [(gcomp/button-expand-child "Okresy" :onClick (fn [e] ((state/state :jarman-views-service) :set-view :view-id "okresy" :title "Okresy" :component-fn create-period-view)))])]
                                        (create-expand-btns--confgen)
                                        [(gcomp/button-expand "Debug items"
                                                        [;;(button-expand-child "Popup" :onClick (fn [e] (@popup-menager :new-message :title "Hello popup panel" :body (c/label "Hello popup!") :size [400 200])))

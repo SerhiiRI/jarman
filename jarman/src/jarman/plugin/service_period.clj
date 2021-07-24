662e72491d3aa826a19c6ee9a64ff23fa277c484
Julia47 <julia.burmich@gmail.com>
2021-07-24 21:51:27 +0300

    update plugin service-period

--- jarman/src/jarman/plugin/service_period.clj
+++ jarman/src/jarman/plugin/service_period.clj
@@ -21,7 +21,51 @@
    [jarman.plugin.data-toolkit :as query-toolkit]
    [jarman.resource-lib.icon-library :as icon]))
 
+(defn- panel-one-period [model-data]
+  (let [label-fn     (fn [text foreground] (seesaw.core/label :text (str text)
+                                                              :font (gtool/getFont 16)
+                                                              :foreground foreground))
+        {st-date         :service_month_start 
+         end-date        :service_month_end
+         payment-st      :service_contract_month.was_payed
+         payment         :money_per_month}  model-data
+        date         (str st-date " / " end-date)
+        status       (if payment-st "It was payed, you cannot change this field" "Waiting for payment")
+        panel        (seesaw.mig/mig-panel :constraints ["wrap 2" "5px[]30px" "10px[]10px"]
+                                           :items [[(label-fn "period:" gcomp/dark-grey-color)]
+                                                   [(label-fn date gcomp/blue-color)]
+                                                   [(label-fn "payment:" gcomp/dark-grey-color)]
+                                                   [(if payment-st (label-fn payment gcomp/blue-color)
+                                                        (gcomp/input-text :args [:columns 7]))]
+                                                   [(label-fn "payment status:" gcomp/dark-grey-color)]
+                                                   [(label-fn status gcomp/blue-color)]])]
+    (gcomp/min-scrollbox panel :hscroll :never)))
 
+
+(defn- panel-one-contract-periods [data]
+  (let [btn-panel (gcomp/menu-bar
+                   {:justify-end true
+                    :buttons [[" Save"
+                               icon/agree1-blue-64-png
+                               (fn [e])]
+                              [" Revert changes"
+                               icon/refresh-blue1-64-png
+                               (fn [e])]
+                              [" Export ODT"
+                               icon/txt-64-png
+                               (fn [e])]
+                              [" Export CSV"
+                               icon/csv-64-png
+                               (fn [e])]]})
+        panel     (seesaw.mig/mig-panel :constraints ["wrap 1" "15px[]15px" "15px[]15px"]
+                                        :items [[btn-panel]])]
+    (doall (map (fn [period] (.add panel (panel-one-period period))) data))
+    (.revalidate panel)
+    (.repaint panel)   
+    panel))
+
+
+ 
 (defn create-period--period-form
   []
   (gcomp/vmig
@@ -76,19 +120,28 @@
                  list-space
                  [return
                   (gcomp/min-scrollbox panel
-                   :hscroll :never)]
+                                       :hscroll :never)]
                  :max-w 180
                  :args [:id :expand-panel :background "#fff"])]
     (doall (map (fn [period] (.add panel (gcomp/button-slim (str (date-fn (:service_contract.contract_start_term period)) " / "
                                                                  (date-fn (:service_contract.contract_end_term period)))
-                                                            :onClick (fn [e] (c/config! view-space :items (gtool/join-mig-items (create-period--period-form))))))) periods-model))
-    (println "SELECT CONTRACTS" periods-model)
+                                                            :onClick (fn [e] (c/config! view-space :items (gtool/join-mig-items
+                                                                                                           (panel-one-contract-periods
+                                                                                                            [{:id_service_contract 2
+                                                                                                              :service_month_start "2020-09-30"
+                                                                                                              :service_month_end "2021-09-30"
+                                                                                                              :money_per_month 400
+                                                                                                              :service_contract_month.was_payed false}
+
+                                                                                                             {:id_service_contract 3
+                                                                                                              :service_month_start "2020-09-28"
+                                                                                                              :service_month_end "2018-02-12"
+                                                                                                              :money_per_month 600
+                                                                                                              :service_contract_month.was_payed true}]
+                                                                                                            ))))))) periods-model))
     (gtool/set-focus return)
     return
     periods))
-
-
-
 
 ;; (defn get-company-list
 ;;   [select-fn]
@@ -122,7 +175,6 @@
      :focus-color "#444"
      :args [:background "#fff"])))
 
-
 (defn create-period-view
   [state]
   (let [list-space (gcomp/vmig :args [:border (b/line-border :left 1 :right 1 :color "#eee")])
@@ -137,17 +189,17 @@
 (defn service-period-toolkit-pipeline [configuration]
   {:enterpreneur-query
    (fn [] (db/query (select! {:table_name :enterpreneur,
-                             :column [:enterpreneur.id
-                                      :enterpreneur.ssreou]})))
+                              :column [:enterpreneur.id
+                                       :enterpreneur.ssreou]})))
    :service_contract-query
    (fn [enterpreneur-id] (db/query (select! {:table_name :service_contract,
-                                              :inner-join
-                                              [:service_contract->enterpreneur],
-                                              :column
-                                              [:#as_is
-                                               :service_contract.contract_start_term
-                                               :service_contract.contract_end_term]
-                                              :where [:= enterpreneur-id :id_enterpreneur]})))})
+                                             :inner-join
+                                             [:service_contract->enterpreneur],
+                                             :column
+                                             [:#as_is
+                                              :service_contract.contract_start_term
+                                              :service_contract.contract_end_term]
+                                             :where [:= enterpreneur-id :id_enterpreneur]})))})
 
 (defn service-period-entry [plugin-path global-configuration-getter]
   (create-period-view

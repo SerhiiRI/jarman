2b632c362346f36e27404e30da1e70ec27dc1475
SerhiiRi <sergiy19981@gmail.com>
2021-08-04 03:17:46 +0200

    delete old todo's

--- jarman/src/jarman/core.clj
+++ jarman/src/jarman/core.clj
@@ -8,19 +8,3 @@
 ;;   [& args]
 ;;   (println "Hello, World!")
 ;;    (log/start))
-
-
-;; ** Aleks [0/6]
-;;     - [ ] dialog box
-;;     - [ ] insert test
-;;     - [x] config generator not open panel
-;;     - [ ] config generator need segment validation
-;;     - [ ] okres plugin to jspl
-;;     - [ ] add export document configuration to jarman-table-plugin + UI spinner or other
-;;     - [ ] test import documnet module
-
-;; ** Serhii [3/4]
-;;     - [x] rewrite jarman-table template for defview
-;;     - [ ] jdbc export controller
-;;     - [x] defview to new jarman-table 2.0
-;;     - [x] many plugin resolver to defview

b36be156d76a6ac4ab2cd7c1ea31037c5d3521f2
Julia47 <julia.burmich@gmail.com>
2021-08-12 01:43:02 +0300

    created composite-components

---
+++ jarman/.jarman.d/plugins/table/composite_components.clj
@@ -0,0 +1,94 @@
+(ns jarman.plugins.composite-components
+  (:require
+   [clojure.string :as s]
+   [jarman.gui.gui-tools :as gtool]
+   [jarman.gui.gui-components :as gcomp]
+   [jarman.logic.composite-components :as ccomp]))
+
+(defn build-test-panel [data-vec onClick]
+  (let [mig (seesaw.mig/mig-panel
+             :background  "#e4e4e4"
+             :constraints ["wrap 2" "10px[95:, fill, grow]10px" "10px[]10px"])]
+    (doall (map (fn [item] (.add mig item)) data-vec))
+    ;;(.add mig (gcomp/button-slim "set data" :onClick onClick) "span 2, align r")
+    (.revalidate mig)
+    (.repaint mig)
+    mig))
+
+(defn link-test   [state-changer link-defr]
+  (let [label-fn   (fn [text] (seesaw.core/label :text text :font (gtool/getFont)))
+        title-txt  (gcomp/state-input-text {:func (fn [e]) :val (:text link-defr)})
+        url-txt    (gcomp/state-input-text {:func (fn [e]) :val (:link link-defr)})]
+    (fn [e] (state-changer (ccomp/->Link
+                            (seesaw.core/text title-txt)
+                            (seesaw.core/text url-txt))))
+    (seesaw.mig/mig-panel
+     :background  "#e4e4e4"
+     :constraints ["wrap 1" "10px[95:, fill, grow]10px" "10px[]10px"]
+     :items [[(label-fn "text:")] [title-txt] [(label-fn "url:")] [url-txt]])))
+
+(defn file-test [state-changer file-defr]
+  (let [label-fn   (fn [text] (seesaw.core/label :text text :font (gtool/getFont)))
+        input-fn   (fn [defr-key] (gcomp/state-input-text {:func (fn [e]) :val (defr-key file-defr)}))  
+        name-txt   (input-fn :file-name)
+        path-txt   (gcomp/status-input-file {:func (fn [e])
+                                             :val (:file-path file-defr)})
+        panel     (build-test-panel [(label-fn "file-name:") (label-fn "file-path:") name-txt path-txt]
+                                    (fn [e] (state-changer (ccomp/->File
+                                                            (seesaw.core/text name-txt)
+                                                            (seesaw.core/text path-txt)))))]
+    (seesaw.core/config! path-txt :listen [:property-change (fn [e]
+                                                              (let [file-name (.getName (clojure.java.io/file (.getToolTipText path-txt) #"/"))]
+                                                                (if-not (empty? file-name)
+                                                                  (do (seesaw.core/config! name-txt :text file-name)
+                                                                      (.revalidate panel)
+                                                                      (.repaint panel))
+                                                                  (seesaw.core/config! name-txt :text ""))))]) panel))
+
+(defn ftp-test [state-changer ftp-defr]
+  (let [label-fn   (fn [text] (seesaw.core/label :text text :font (gtool/getFont)))
+        input-fn   (fn [defr-key] (gcomp/state-input-text {:func (fn [e]) :val (defr-key ftp-defr)}))
+        login-txt  (input-fn :login)
+        passw-txt  (input-fn :password)
+        name-txt   (input-fn :file-name)
+        path-txt   (gcomp/status-input-file {:func (fn [e])
+                                             :val (:file-path ftp-defr)})
+        panel     (build-test-panel [(label-fn "login:") (label-fn "password:") login-txt passw-txt
+                                     (label-fn "file-name:") (label-fn "file-path:") name-txt path-txt]
+                                    (fn [e] (state-changer (ccomp/->FtpFile
+                                                            (seesaw.core/text login-txt)
+                                                            (seesaw.core/text passw-txt) ;;change logic to state
+                                                            (seesaw.core/text name-txt)
+                                                            (seesaw.core/text path-txt)))))]
+    (seesaw.core/config! path-txt :listen [:property-change (fn [e]
+                                                              (let [file-name  (last (s/split (.getToolTipText path-txt) #"/"))]
+                                                                (if-not (empty? file-name)
+                                                                  (do (seesaw.core/config! name-txt :text file-name)
+                                                                      (.revalidate panel)
+                                                                      (.repaint panel))
+                                                                  (seesaw.core/config! name-txt :text ""))))]) panel)) ;;TO DO change logic to state
+
+(comment (let [state (atom {})
+               mig   (fn [items](seesaw.core/scrollable (seesaw.mig/mig-panel
+                                                         :constraints ["wrap 1" "150px[]0px" "100px[]0px"]
+                                                         :items [[items]])))
+               panel-file (mig (file-test  #(swap! state (fn [s] (assoc-in s [:ftp-defr %])) [])
+                                                                      (File. "panda" "some-path/heyy/file.txt")))
+               panel-link (mig (link-test #(swap! state (fn [s] (assoc-in s [:link-defr %]))  [])
+                                          (Link. "Just click" "https://www.yoyube")))
+               panel-ftp  (mig (ftp-test  #(swap! state (fn [s] (assoc-in s [:ftp-defr %])) [])
+                                                                      (FtpFile. "trashpanda" "123" "panda" "some-path/heyy/file.txt")))]
+           (-> (doto (seesaw.core/frame
+                      :title "DEBUG WINDOW" :undecorated? false
+                      :minimum-size [450 :by 450]
+                      :size [450 :by 450]
+                      :content panel-link
+                     ;; :content panel-ftp
+                      )
+                 (.setLocationRelativeTo nil) seesaw.core/pack! seesaw.core/show!))))
+
+
+ ;; :content (link-test #(swap! state (fn [s] (assoc-in s [:link-defr %]))  [])
+                      ;;                           (Link. "Just click" "https://www.youtube.com/"))
+(seesaw.dev/show-events (seesaw.core/label))
+  :component-moved
---
+++ jarman/src/jarman/logic/composite_components.clj
@@ -0,0 +1,116 @@
+(ns jarman.logic.composite-components
+  (:require [miner.ftp :as ftp]
+            [jarman.logic.session :refer [get-user-configuration]]))
+;;;;;;;;;;;;;;;;;;;;;
+;;; FTP FUNCTIONS ;;;
+;;;;;;;;;;;;;;;;;;;;;
+(defn copy [uri file]
+  (with-open [in (clojure.java.io/input-stream uri)
+              out (clojure.java.io/output-stream file)]
+    (clojure.java.io/copy in out)))
+
+(defn ftp-list-files [repo-url]
+  (ftp/with-ftp [client repo-url]
+    (ftp/client-cd client "jarman")
+    (ftp/client-all-names client)))
+
+(defn ftp-put-file [ftp-repo-url repo-path file-path ] 
+  (ftp/with-ftp [client ftp-repo-url]
+    (ftp/client-cd client repo-path)))
+
+ 
+
+(defn ftp-get-file
+  ([repo-url file-name]
+   (do (copy (clojure.string/join "/" [repo-url "jarman" file-name]) file-name))))
+
+;;(ftp-get-file "ftp://jarman:dupa@trashpanda-team.ddns.net" "jarman.txt")
+;;(.exists (clojure.java.io/file "/home/julia/test.png"))
+;;(ftp-list-files "ftp://jarman:dupa@trashpanda-team.ddns.net")
+;; (ftp-put-file "ftp://jarman:dupa@trashpanda-team.ddns.net"
+;;               "/db/user/"
+;;               "/home/julia/test.png")
+;;show all files in directory
+;;download file
+;;upload file
+;;;;;;;;;;;;;;;;;;;;;
+
+(defprotocol IStorage
+  (load-session-config [this]))
+
+(defprotocol IFtpQuery
+  (get-list-files [this])
+  (get-file-ftp [this])
+  (put-file-ftp [this]))
+
+(defrecord FtpFile [login password file-name file-path]
+  IStorage
+  (load-session-config [this]
+    (let [{:keys [login, password, host]}
+          (get-in (get-user-configuration) [:ftp] {})]
+      (format "ftp://%s:%s@%s" login, password, host)))
+  IFtpQuery
+  (get-list-files [this]
+    (ftp-list-files (load-session-config this)))
+  (get-file-ftp [this]
+    (ftp-get-file (load-session-config this) file-name))
+  (put-file-ftp [this]
+    (ftp-put-file (load-session-config this) "/bd/user"  file-path)))
+
+(defn isFtp? [^jarman.logic.composite_components.FtpFile e]
+  (instance? jarman.logic.composite_components.FtpFile e))
+
+(defn ftp-file [login password file-name file-path]
+  {:pre [(every? string? (list login password file-name file-path))]}
+  (->FtpFile login password file-name file-path))
+
+;;(.get-list-files (ftp-file "" "" "" ""))
+
+;;ftp://jarman:dupa@trashpanda-team.ddns.net
+
+
+
+
+;; (clojure.java.browse/browse-url "http://clojuredocs.org")
+(defrecord Link [text link])
+;; => jarman.plugin.agr_col_test.Link
+(defn isLink? [^jarman.logic.composite_components.Link e]
+  (instance? jarman.logic.composite_components.Link e))
+
+;;(if (isLink? col) (build-panel (fn) col))
+(defrecord File [file-name file-path])
+
+(comment
+  (defprotocol IPluginQuery
+    (return-path [this])
+    (return-entry [this])
+    (return-permission [this])
+    (return-title [this])
+    (return-config [this])
+    (exists? [this]))
+  
+  (defrecord PluginLink [plugin-path]
+    IPluginQuery
+    (return-path [this] (.plugin-path this))
+    (return-entry [this] (get-in (global-view-configs-get) (conj (return-path this) :entry)))
+    (return-title [this] (get-in (global-view-configs-get) (conj (return-path this) :config :name)))
+    (return-permission [this] (get-in (global-view-configs-get) (conj (return-path this) :config :permission)))
+    (return-config [this] (get-in (global-view-configs-get) (conj (return-path this) :config)))
+    (exists? [this] (some? (get-in (global-view-configs-get) (return-path this)))))
+
+  (defn isPluginLink? [^jarman.logic.view_manager.PluginLink e]
+    (instance? jarman.logic.view_manager.PluginLink e))
+
+  (defn plugin-link [^clojure.lang.PersistentVector plugin-path]
+    {:pre [(every? keyword? plugin-path)]}
+    (->PluginLink plugin-path)))
+
+(comment
+  ;; test segment for some link
+  (def --test-link (plugin-link [:user :table :user]))
+  (.return-path --test-link)
+  (.return-entry --test-link)
+  (.return-permission --test-link)
+  (.return-title --test-link)
+  (.return-config --test-link)
+  (.exists? --test-link))

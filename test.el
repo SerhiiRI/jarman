e0932220bc595d2efe17ff002dbf99d33f36437b
Serhii Riznychuk <sergiy19981@gmail.com>
2021-09-14 09:54:15 +0200

    implement translations

--- test.el
+++ test.el
@@ -76,3 +76,27 @@
 (select-frame-by-name "MAIN") (find-file "~/org/files/agenda/TODO.org")
 
 #+END_SRC
+
+
+
+
+
+
+
+;;;;;;;;;;
+;; dfso ;;
+;;;;;;;;;;
+
+(defun jarman-comment-box (b e)
+  "Draw a box comment around the region but arrange for the region to extend to at least the fill column. Place the point after the comment box."
+
+  (interactive "r")
+  (let ((e (copy-marker e t)))
+    (goto-char b)
+    (end-of-line)
+    (insert-char ?  (- fill-column (current-column)))
+    (comment-box b e 1)
+    (goto-char e)
+    (set-marker e nil)))
+
+(global-set-key (kbd "C-c b b") 'bjm-comment-box)

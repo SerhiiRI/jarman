f2fb975e68856b47e0540b2669159822d36b43cd
Aleks <alekses404@gmail.com>
2021-10-20 14:02:34 +0200

    +Updated Agenda +Added few tests

--- jarman/.jarman.d/plugins/service_period/service_period.clj
+++ jarman/.jarman.d/plugins/service_period/service_period.clj
@@ -61,15 +61,6 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; HELPER FUNCTIONS ;;;  
 ;;;;;;;;;;;;;;;;;;;;;;;;
-(defn- format-date [date]
-  (let [fn-format (fn [date] (.format (java.text.SimpleDateFormat. "dd-MM-YYYY") date))]
-    (if (or (vector? date)(seq? date))
-      (map (fn [d] (fn-format d)) date)
-      (fn-format date))))
-
-(defn- split-date [date-vec]
-  (apply str (interpose " / " (format-date date-vec))))
-
 (defn- isNumber? [val-s]
   (try (float (read-string val-s))
     (catch Exception e false)))
@@ -257,6 +248,12 @@
     (if (or (float? contract-price) (int? contract-price))
       contract-price 0)))
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;; INFO BAR
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 (defn- info-bar-params
   "Description:
      Data for info bar for contract view. Add some more, it's not any problem.
@@ -279,9 +276,9 @@
   "Description:
      Create info bar content in columns. Default 3 columns."
   [state! enterprise contract-price date-start date-end collumns-count]
-  (let [font-size 15
+  (let [font-size 14
         cc (* 2 collumns-count)
-        collumns-v (map (fn [idx](gmg/migrid :v :f {:gap (if (not (even? idx)) [5 5 5 40] [5])} [])) (take cc (range)))
+        collumns-v (map (fn [idx](gmg/migrid :v :f {:gap (if (not (even? idx)) [5 5 5 30] [5])} [])) (take cc (range)))
         counter    (atom 0)
         label-fn   (fn [txt & {:keys [bold]}]
                      (c/label :text txt :font (if bold (gs/getFont :bold font-size) (gs/getFont font-size))))]
@@ -315,7 +312,7 @@
              (fn [key atom old-m new-m]
                (c/config! panel :items (gtool/join-mig-items (render-fn)))
                (.repaint panel)))
-    panel))
+    (gcomp/min-scrollbox panel)))
 
 (defn- panel-with-subcontract-rows
   "Description
@@ -910,3 +907,372 @@
  :entry service-period-entry
  :toolkit service-period-toolkit-pipeline
  :spec-list [])
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;; TESTS
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; (def test-enterprises-m
+;;  {:1
+;;   {:enterprise.director "Ivan Ivankow",
+;;    :enterprise.individual_tax_number "3323392190",
+;;    :enterprise.accountant "Anastasia Wewbytska",
+;;    :enterprise.contacts_information "306690666",
+;;    :enterprise.vat_certificate "EKCA31232",
+;;    :enterprise.physical_address "B1",
+;;    :enterprise.ssreou "32432432",
+;;    :enterprise.ownership_form "LTD",
+;;    :enterprise.id 1,
+;;    :enterprise.legal_address "A1",
+;;    :enterprise.name "Biedronka"},
+;;   :3
+;;   {:enterprise.director "Vasyl Mayni",
+;;    :enterprise.individual_tax_number "2131248412",
+;;    :enterprise.accountant "Aleksand",
+;;    :enterprise.contacts_information "306690666",
+;;    :enterprise.vat_certificate "UKCP12394",
+;;    :enterprise.physical_address "B2",
+;;    :enterprise.ssreou "11134534",
+;;    :enterprise.ownership_form "PP",
+;;    :enterprise.id 3,
+;;    :enterprise.legal_address "A2",
+;;    :enterprise.name "some shop"},
+;;   :2
+;;   {:enterprise.director "Vasyl Mayni",
+;;    :enterprise.individual_tax_number "2312931424",
+;;    :enterprise.accountant "Aleksand",
+;;    :enterprise.contacts_information "306690666",
+;;    :enterprise.vat_certificate "EKCP12344",
+;;    :enterprise.physical_address "B2",
+;;    :enterprise.ssreou "23155555",
+;;    :enterprise.ownership_form "PP",
+;;    :enterprise.id 2,
+;;    :enterprise.legal_address "A2",
+;;    :enterprise.name "KFC"}})
+
+;; (def test-contracts-m
+;;   {:1
+;;    {:21
+;;     {:service_contract.id 21,
+;;      :service_contract.contract_start_term
+;;      #inst "2021-10-15T22:00:00.000-00:00",
+;;      :service_contract.contract_end_term
+;;      #inst "2021-12-30T23:00:00.000-00:00",
+;;      :selected? false},
+;;     :23
+;;     {:service_contract.id 23,
+;;      :service_contract.contract_start_term
+;;      #inst "2021-10-31T23:00:00.000-00:00",
+;;      :service_contract.contract_end_term
+;;      #inst "2022-10-30T23:00:00.000-00:00",
+;;      :selected? false}},
+;;    :3
+;;    {:22
+;;     {:service_contract.id 22,
+;;      :service_contract.contract_start_term
+;;      #inst "2021-10-15T22:00:00.000-00:00",
+;;      :service_contract.contract_end_term
+;;      #inst "2021-10-30T22:00:00.000-00:00",
+;;      :selected? false}},
+;;    :2
+;;    {:24
+;;     {:service_contract.id 24,
+;;      :service_contract.contract_start_term
+;;      #inst "2021-10-31T23:00:00.000-00:00",
+;;      :service_contract.contract_end_term
+;;      #inst "2021-12-30T23:00:00.000-00:00",
+;;      :selected? false},
+;;     :25
+;;     {:service_contract.id 25,
+;;      :service_contract.contract_start_term
+;;      #inst "2021-10-18T22:00:00.000-00:00",
+;;      :service_contract.contract_end_term
+;;      #inst "2021-11-18T23:00:00.000-00:00",
+;;      :selected? false}}})
+
+;; (def test-subcontracts-m
+;;  {:1
+;;   {:21
+;;    {:1021
+;;     {:service_contract_month.id 1021,
+;;      :service_contract_month.service_month_start
+;;      #inst "2021-10-15T22:00:00.000-00:00",
+;;      :service_contract_month.service_month_end
+;;      #inst "2021-10-30T22:00:00.000-00:00",
+;;      :service_contract_month.money_per_month 150.0,
+;;      :service_contract_month.was_payed true,
+;;      :selected? false},
+;;     :1022
+;;     {:service_contract_month.id 1022,
+;;      :service_contract_month.service_month_start
+;;      #inst "2021-10-31T23:00:00.000-00:00",
+;;      :service_contract_month.service_month_end
+;;      #inst "2021-11-29T23:00:00.000-00:00",
+;;      :service_contract_month.money_per_month 100.0,
+;;      :service_contract_month.was_payed false,
+;;      :selected? false},
+;;     :1023
+;;     {:service_contract_month.id 1023,
+;;      :service_contract_month.service_month_start
+;;      #inst "2021-11-30T23:00:00.000-00:00",
+;;      :service_contract_month.service_month_end
+;;      #inst "2021-12-30T23:00:00.000-00:00",
+;;      :service_contract_month.money_per_month 300.0,
+;;      :service_contract_month.was_payed false,
+;;      :selected? false}},
+;;    :23
+;;    {:1025
+;;     {:service_contract_month.id 1025,
+;;      :service_contract_month.service_month_start
+;;      #inst "2021-10-31T23:00:00.000-00:00",
+;;      :service_contract_month.service_month_end
+;;      #inst "2021-11-29T23:00:00.000-00:00",
+;;      :service_contract_month.money_per_month 10.0,
+;;      :service_contract_month.was_payed true,
+;;      :selected? false},
+;;     :1029
+;;     {:service_contract_month.id 1029,
+;;      :service_contract_month.service_month_start
+;;      #inst "2022-02-28T23:00:00.000-00:00",
+;;      :service_contract_month.service_month_end
+;;      #inst "2022-03-30T22:00:00.000-00:00",
+;;      :service_contract_month.money_per_month 10.0,
+;;      :service_contract_month.was_payed true,
+;;      :selected? false},
+;;     :1031
+;;     {:service_contract_month.id 1031,
+;;      :service_contract_month.service_month_start
+;;      #inst "2022-04-30T22:00:00.000-00:00",
+;;      :service_contract_month.service_month_end
+;;      #inst "2022-05-30T22:00:00.000-00:00",
+;;      :service_contract_month.money_per_month 10.0,
+;;      :service_contract_month.was_payed false,
+;;      :selected? false},
+;;     :1027
+;;     {:service_contract_month.id 1027,
+;;      :service_contract_month.service_month_start
+;;      #inst "2020-12-31T23:00:00.000-00:00",
+;;      :service_contract_month.service_month_end
+;;      #inst "2022-01-30T23:00:00.000-00:00",
+;;      :service_contract_month.money_per_month 10.0,
+;;      :service_contract_month.was_payed false,
+;;      :selected? false},
+;;     :1030
+;;     {:service_contract_month.id 1030,
+;;      :service_contract_month.service_month_start
+;;      #inst "2022-03-31T22:00:00.000-00:00",
+;;      :service_contract_month.service_month_end
+;;      #inst "2022-04-29T22:00:00.000-00:00",
+;;      :service_contract_month.money_per_month 10.0,
+;;      :service_contract_month.was_payed false,
+;;      :selected? false},
+;;     :1026
+;;     {:service_contract_month.id 1026,
+;;      :service_contract_month.service_month_start
+;;      #inst "2021-11-30T23:00:00.000-00:00",
+;;      :service_contract_month.service_month_end
+;;      #inst "2021-12-30T23:00:00.000-00:00",
+;;      :service_contract_month.money_per_month 10.0,
+;;      :service_contract_month.was_payed false,
+;;      :selected? false},
+;;     :1036
+;;     {:service_contract_month.id 1036,
+;;      :service_contract_month.service_month_start
+;;      #inst "2022-09-30T22:00:00.000-00:00",
+;;      :service_contract_month.service_month_end
+;;      #inst "2022-10-30T23:00:00.000-00:00",
+;;      :service_contract_month.money_per_month 10.0,
+;;      :service_contract_month.was_payed false,
+;;      :selected? false},
+;;     :1032
+;;     {:service_contract_month.id 1032,
+;;      :service_contract_month.service_month_start
+;;      #inst "2022-05-31T22:00:00.000-00:00",
+;;      :service_contract_month.service_month_end
+;;      #inst "2022-06-29T22:00:00.000-00:00",
+;;      :service_contract_month.money_per_month 10.0,
+;;      :service_contract_month.was_payed false,
+;;      :selected? false},
+;;     :1028
+;;     {:service_contract_month.id 1028,
+;;      :service_contract_month.service_month_start
+;;      #inst "2022-01-31T23:00:00.000-00:00",
+;;      :service_contract_month.service_month_end
+;;      #inst "2022-02-27T23:00:00.000-00:00",
+;;      :service_contract_month.money_per_month 10.0,
+;;      :service_contract_month.was_payed false,
+;;      :selected? false},
+;;     :1033
+;;     {:service_contract_month.id 1033,
+;;      :service_contract_month.service_month_start
+;;      #inst "2022-06-30T22:00:00.000-00:00",
+;;      :service_contract_month.service_month_end
+;;      #inst "2022-07-30T22:00:00.000-00:00",
+;;      :service_contract_month.money_per_month 10.0,
+;;      :service_contract_month.was_payed false,
+;;      :selected? false},
+;;     :1035
+;;     {:service_contract_month.id 1035,
+;;      :service_contract_month.service_month_start
+;;      #inst "2022-08-31T22:00:00.000-00:00",
+;;      :service_contract_month.service_month_end
+;;      #inst "2022-09-29T22:00:00.000-00:00",
+;;      :service_contract_month.money_per_month 10.0,
+;;      :service_contract_month.was_payed false,
+;;      :selected? false},
+;;     :1034
+;;     {:service_contract_month.id 1034,
+;;      :service_contract_month.service_month_start
+;;      #inst "2022-07-31T22:00:00.000-00:00",
+;;      :service_contract_month.service_month_end
+;;      #inst "2022-08-30T22:00:00.000-00:00",
+;;      :service_contract_month.money_per_month 10.0,
+;;      :service_contract_month.was_payed false,
+;;      :selected? false}}},
+;;   :3
+;;   {:22
+;;    {:1024
+;;     {:service_contract_month.id 1024,
+;;      :service_contract_month.service_month_start
+;;      #inst "2021-10-15T22:00:00.000-00:00",
+;;      :service_contract_month.service_month_end
+;;      #inst "2021-10-30T22:00:00.000-00:00",
+;;      :service_contract_month.money_per_month 50.0,
+;;      :service_contract_month.was_payed true,
+;;      :selected? false}}},
+;;   :2
+;;   {:24
+;;    {:1037
+;;     {:service_contract_month.id 1037,
+;;      :service_contract_month.service_month_start
+;;      #inst "2021-10-31T23:00:00.000-00:00",
+;;      :service_contract_month.service_month_end
+;;      #inst "2021-11-29T23:00:00.000-00:00",
+;;      :service_contract_month.money_per_month 10.0,
+;;      :service_contract_month.was_payed true,
+;;      :selected? false},
+;;     :1038
+;;     {:service_contract_month.id 1038,
+;;      :service_contract_month.service_month_start
+;;      #inst "2021-11-30T23:00:00.000-00:00",
+;;      :service_contract_month.service_month_end
+;;      #inst "2021-12-30T23:00:00.000-00:00",
+;;      :service_contract_month.money_per_month 10.0,
+;;      :service_contract_month.was_payed true,
+;;      :selected? false}},
+;;    :25
+;;    {:1039
+;;     {:service_contract_month.id 1039,
+;;      :service_contract_month.service_month_start
+;;      #inst "2021-10-18T22:00:00.000-00:00",
+;;      :service_contract_month.service_month_end
+;;      #inst "2021-10-30T22:00:00.000-00:00",
+;;      :service_contract_month.money_per_month 20.0,
+;;      :service_contract_month.was_payed false,
+;;      :selected? false},
+;;     :1040
+;;     {:service_contract_month.id 1040,
+;;      :service_contract_month.service_month_start
+;;      #inst "2021-10-31T23:00:00.000-00:00",
+;;      :service_contract_month.service_month_end
+;;      #inst "2021-11-18T23:00:00.000-00:00",
+;;      :service_contract_month.money_per_month 20.0,
+;;      :service_contract_month.was_payed false,
+;;      :selected? false}}}})
+
+;; (def ok-subcontracts-all-selected-select
+;;   {:1021
+;;    {:service_contract_month.id 1021,
+;;     :service_contract_month.service_month_start
+;;     #inst "2021-10-15T22:00:00.000-00:00",
+;;     :service_contract_month.service_month_end
+;;     #inst "2021-10-30T22:00:00.000-00:00",
+;;     :service_contract_month.money_per_month 150.0,
+;;     :service_contract_month.was_payed true,
+;;     :selected? true},
+;;    :1022
+;;    {:service_contract_month.id 1022,
+;;     :service_contract_month.service_month_start
+;;     #inst "2021-10-31T23:00:00.000-00:00",
+;;     :service_contract_month.service_month_end
+;;     #inst "2021-11-29T23:00:00.000-00:00",
+;;     :service_contract_month.money_per_month 100.0,
+;;     :service_contract_month.was_payed false,
+;;     :selected? true},
+;;    :1023
+;;    {:service_contract_month.id 1023,
+;;     :service_contract_month.service_month_start
+;;     #inst "2021-11-30T23:00:00.000-00:00",
+;;     :service_contract_month.service_month_end
+;;     #inst "2021-12-30T23:00:00.000-00:00",
+;;     :service_contract_month.money_per_month 300.0,
+;;     :service_contract_month.was_payed false,
+;;     :selected? true}})
+
+;; (def ok-subcontracts-all-selected-unselect
+;;   {:1021
+;;    {:service_contract_month.id 1021,
+;;     :service_contract_month.service_month_start
+;;     #inst "2021-10-15T22:00:00.000-00:00",
+;;     :service_contract_month.service_month_end
+;;     #inst "2021-10-30T22:00:00.000-00:00",
+;;     :service_contract_month.money_per_month 150.0,
+;;     :service_contract_month.was_payed true,
+;;     :selected? false},
+;;    :1022
+;;    {:service_contract_month.id 1022,
+;;     :service_contract_month.service_month_start
+;;     #inst "2021-10-31T23:00:00.000-00:00",
+;;     :service_contract_month.service_month_end
+;;     #inst "2021-11-29T23:00:00.000-00:00",
+;;     :service_contract_month.money_per_month 100.0,
+;;     :service_contract_month.was_payed false,
+;;     :selected? false},
+;;    :1023
+;;    {:service_contract_month.id 1023,
+;;     :service_contract_month.service_month_start
+;;     #inst "2021-11-30T23:00:00.000-00:00",
+;;     :service_contract_month.service_month_end
+;;     #inst "2021-12-30T23:00:00.000-00:00",
+;;     :service_contract_month.money_per_month 300.0,
+;;     :service_contract_month.was_payed false,
+;;     :selected? false}})
+
+;; (defn- initial-test-state-template [state]
+;;   (reset!
+;;    state
+;;    {:enterprises-m        (atom {})
+;;     :contracts-m          (atom {})
+;;     :subcontracts-m       (atom {})
+;;     :currency             "UAH"
+;;     :subcontracts-payment-state (atom {})}))
+
+;; (defn initialize-test-state [state!]
+;;   (reset! (:enterprises-m  (state!)) test-enterprises-m)
+;;   (reset! (:contracts-m    (state!)) test-contracts-m)
+;;   (reset! (:subcontracts-m (state!)) test-subcontracts-m))
+
+;; (defn run-tests []
+;;   (println "\nInitialize Tests:")
+;;   (let [test-state  (atom {})
+;;         test-state! (deref test-state)]
+;;     (initial-test-state-template test-state)
+;;     (initialize-test-state test-state!)
+;;     (->>
+;;      (-> []
+;;          (concat [[:calculate-contract-price<nopay>     (= 400.0 (calculate-contract-price test-state! [:1 :21]))]])
+;;          (concat [[:calculate-contract-price<payed>     (= 0     (calculate-contract-price test-state! [:3 :22]))]])
+;;          (concat [[:calculate-all-contract-price<nopay> (= 40.0  (calculate-all-contract-price test-state! [:2]))]])
+;;          (concat [[:calculate-all-contract-price<payed> (= 0     (calculate-all-contract-price test-state! [:3]))]])
+;;          (concat [[:select-all-subcontracts<select>     (= (select-all-subcontracts test-state! [:1 :21] true)  ok-subcontracts-all-selected-select)]])
+;;          (concat [[:select-all-subcontracts<unselect>   (= (select-all-subcontracts test-state! [:1 :21] false) ok-subcontracts-all-selected-unselect)]])
+;;          (concat [[:check-if-contract-selected<selected>    (= true (check-if-contract-selected test-state! [:3 :2]))]])
+;;          (concat [[:check-if-contract-selected<notselected> (= false (check-if-contract-selected test-state! [:1 :21]))]]))
+;;      (map #(do (println "TEST: " (if (second %) "OK" "FAILD") " - " (first %)) (second %))))))
+
+;;(run-tests)
+
+

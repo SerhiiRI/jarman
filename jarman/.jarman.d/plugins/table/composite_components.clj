9947b8b8a8252e0453c93c19ff6735ec77cd79c3
SerhiiRi <sergiy19981@gmail.com>
2022-01-10 16:45:56 +0100

    WIP table.clj: fix up starting problem

--- jarman/.jarman.d/plugins/table/composite_components.clj
+++ jarman/.jarman.d/plugins/table/composite_components.clj
@@ -1,13 +1,13 @@
 (ns plugin.table.composite-components
   (:require
    [clojure.string :as s]
-   [clojure.core.async :as async :refer :all]
    [seesaw.core   :as c]
    [jarman.tools.lang :refer [in?]]
    [jarman.gui.gui-tools :as gtool]
    [jarman.gui.core :refer [register! satom]]
    [jarman.gui.gui-panel :refer [mig-panel]]
    [jarman.gui.gui-components :as gcomp]
+   [jarman.gui.gui-components2]
    [jarman.faces              :as face]
    [jarman.logic.composite-components :as ccomp]
    [jarman.logic.metadata]))
@@ -50,41 +50,43 @@
      :constraints ["wrap 1" "10px[95:, fill, grow]10px" "10px[]10px"]
      :items [[(f-label "text:")] [f-title] [(f-label "url:")] [f-url]])))
 
-(defn url-panel [{:keys [default on-change] :or {on-change (fn [e]) default {}}}]
-    (let [a (satom (jarman.logic.metadata/map->Link default))
-          text-params (fn [k] [:on-change (fn [e] (swap! a assoc k (c/value e)) (on-change @a)) :text (k @a)])]
-      (mig-panel 
-       :background  face/c-compos-background-darker
-       :constraints ["wrap 1" "10px[95:, fill, grow]10px" "10px[]10px"]
-       :items [[(f-label "text:")] [(apply jarman.gui.gui-components2/text (text-params :text))]
-               [(f-label "url:")]  [(apply jarman.gui.gui-components2/text (text-params :link))]])))
+(comment
+  ;; WIP
+ (defn url-panel [{:keys [default on-change] :or {on-change (fn [e]) default {}}}]
+   (let [a (satom (jarman.logic.metadata/map->Link default))
+         text-params (fn [k] [:on-change (fn [e] (swap! a assoc k (c/value e)) (on-change @a)) :text (k @a)])]
+     (mig-panel 
+      :background  face/c-compos-background-darker
+      :constraints ["wrap 1" "10px[95:, fill, grow]10px" "10px[]10px"]
+      :items [[(f-label "text:")] [(apply jarman.gui.gui-components2/text (text-params :text))]
+              [(f-label "url:")]  [(apply jarman.gui.gui-components2/text (text-params :link))]])))
 
-(defn file-panel
-  [{:keys [default on-change on-download mode] :or {on-change (fn [e]) on-download (fn [e]) default {} mode nil}}]
-  (let [state (satom (jarman.logic.metadata/map->File default))
-        unselected-file? (atom true)
-        text-params (fn [k] {:func (fn [e] (swap! sratom assoc k (c/value e)) (on-change @sratom)) :val (k @sratom)})
-        sratom-download (fn [] {:func (fn [e] (on-download @sratom)) :val ""})]
-    (let [l-name     (f-label "File name")
-          l-file     (f-label "File path")
-          l-save     (f-label "Save file")
-          f-name     (gcomp/state-input-text  (sratom-kf :file-name))
-          path-file  (gcomp/status-input-file (sratom-kf :file))
-          path-save  (gcomp/status-input-file (sratom-download))
-          panel      (if (deref unselected-file?)
-                       (build-main-panel [[l-name] [l-file]                    [f-name] [path-file]                      ])
-                       (build-main-panel [[l-name] [l-file "split 2"] [l-save] [f-name] [path-file "split 2"] [path-save]]))]
-      (seesaw.core/config!
-       path-file :listen
-       [:property-change
-        (fn [e]
-          (let [file-name (last (s/split (.getToolTipText path-file) (re-pattern (java.io.File/separator))))]
-            (if-not (empty? file-name)
-              (do (seesaw.core/config! f-name :text file-name)
-                  (.revalidate panel)
-                  (.repaint panel))
-              (seesaw.core/config! f-name :text (:file-name val)))))])
-      panel)))
+ (defn file-panel
+   [{:keys [default on-change on-download mode] :or {on-change (fn [e]) on-download (fn [e]) default {} mode nil}}]
+   (let [state (satom (jarman.logic.metadata/map->File default))
+         unselected-file? (atom true)
+         text-params (fn [k] {:func (fn [e] (swap! sratom assoc k (c/value e)) (on-change @sratom)) :val (k @sratom)})
+         sratom-download (fn [] {:func (fn [e] (on-download @sratom)) :val ""})]
+     (let [l-name     (f-label "File name")
+           l-file     (f-label "File path")
+           l-save     (f-label "Save file")
+           f-name     (gcomp/state-input-text  (sratom-kf :file-name))
+           path-file  (gcomp/status-input-file (sratom-kf :file))
+           path-save  (gcomp/status-input-file (sratom-download))
+           panel      (if (deref unselected-file?)
+                        (build-main-panel [[l-name] [l-file]                    [f-name] [path-file]                      ])
+                        (build-main-panel [[l-name] [l-file "split 2"] [l-save] [f-name] [path-file "split 2"] [path-save]]))]
+       (seesaw.core/config!
+        path-file :listen
+        [:property-change
+         (fn [e]
+           (let [file-name (last (s/split (.getToolTipText path-file) (re-pattern (java.io.File/separator))))]
+             (if-not (empty? file-name)
+               (do (seesaw.core/config! f-name :text file-name)
+                   (.revalidate panel)
+                   (.repaint panel))
+               (seesaw.core/config! f-name :text (:file-name val)))))])
+       panel))))
 
 (comment
   (require 'jarman.gui.gui-components2)

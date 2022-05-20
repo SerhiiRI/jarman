9f10d0b9ab3a73e8660a3fd5c591b1e41ad1dc38
SerhiiRi <sergiy19981@gmail.com>
2022-05-20 02:10:29 +0200

    metacomponets: update global tools, printing and common lang function

--- jarman/src/jarman/tools/lang.clj
+++ jarman/src/jarman/tools/lang.clj
@@ -745,21 +745,13 @@
      (recur (dissoc-in m ks) ks' kss)
      (dissoc-in m ks))))
 
-
-;;; FUZZY SEARCHING
-(comment
-  (require '[clj-fuzzy.metrics :as fuzzy])
-  (let [searching-word "genera"]
-   (->> (return-public-functions 'jarman.logic.security)
-        (mapv (comp name symbol))
-        (mapv #(vector % (fuzzy/levenshtein searching-word %)))
-        (sort-by second))))
-
-
 (defn return-public-functions [ns]
   {:pre [(symbol? ns)]}
   (filter (comp some? :arglists meta)
           (vals (ns-publics ns))))
+
+(defmacro copy-locals [& vars]
+  `(hash-map ~@(mapcat (fn [x] [(keyword (name x)) x]) vars)))
 
 ;;;;;;;;;;;;;;;;
 ;;; REFLECTS ;;;

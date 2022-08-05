e1b1142b54383c2e8a36f57d3b46bcc9f278171e
SerhiiRi <sergiy19981@gmail.com>
2022-08-05 07:21:09 +0200

    master: various refactorization

--- jarman/src/jarman/application/collector_custom_metacomponents.clj
+++ jarman/src/jarman/application/collector_custom_metacomponents.clj
@@ -34,7 +34,10 @@
   (assert (fn? component)           "Component plugin `:component` MUST be a 'clojure.lang.IFn' type")
   (Component. id component actions constructor))
 
-(defn register-metacomponent [& {:as args}]
+(defn register-metacomponent
+  "Example
+   register-metacomponent :id :jsgl-link :component stub :actions (get-comp-actions 'stub)"
+  [& {:as args}]
   (dosync (alter system-Components-list
             (fn [persisted-component-list]
               (blet
@@ -48,8 +51,48 @@
     (apply component (-> component-meta
                        (dissoc :type)
                        ((partial apply concat))))
+    (throw (ex-info (format "Component with `%s` doesn't register in system." (str type))
+             {:metafield component-meta
+              :type type}))))
+
+(defn metafield->component [{{:keys [type] :as component-meta} :component :as metafiled}]
+  (if-let [{:keys [id component actions constructor]} (get (system-Components-list-group-get) type nil)]
+    (apply component (-> component-meta
+                       (dissoc :type)
+                       ((partial apply concat))))
     (throw (ex-info (format "Comopnent with `%s` doesn't register in system." (str type))
              {:metafield component-meta
               :type type}))))
 
 (defn metacomponents-get [] (system-Components-list-group-get))
+
+
+(comment
+  (def v1
+    (seesaw.core/vertical-panel
+      :items [(seesaw.core/label :text "sukafds")]
+      :border (jarman.gui.components.swing/border {:a 10})))
+  (defn suka []
+    (seesaw.core/config!
+      (let [XS [{:field :first_name,
+                 :field-qualified :user.first_name
+                 :component
+                 {:type :jsgl-text, :value "ldjflaskdjlsakjf"
+                  :on-change (fn [e] (println "A")
+                               (seesaw.core/text e))}
+                 :representation "Imie"
+                 :description "No kurwa jak ciebie mamka z tatkiem nazwali"}
+                {:field :last_name,
+                 :field-qualified :user.last_name
+                 :representation "Nazwisko"
+                 :description "Drugie slowo co musisz pamiętać w życiu"
+                 :component
+                 {:type :jsgl-text, :value "fdlaskjlk"
+                  :on-change (fn [e]
+                               (println "B")
+                               (seesaw.core/text e))}}]]
+        (for [X XS]
+          (metacomponent->component (:component X))))))
+  (seesaw.core/config! v1 :items (suka))
+  (jarman.gui.components.swing/quick-frame
+    [v1]))
